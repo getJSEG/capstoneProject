a9f33b9a6244701699a95e5a35e9001f935bda3b
Elmer Gonzalez <elmergonzalez712@gmail.com>
2019-07-29 18:46:17 -0500

    redy for deployment

--- server/index.js
+++ server/index.js
@@ -19,16 +19,9 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 //Static file declaration
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, 'build')));
 
-//production mode
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })
-}
-
-//build mode
-app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
+app.get('/', function(req, res) {   res.sendFile(path.join(__dirname, 'build', 'index.html')); });
 
 //set up cors to allows to accept request from the client
 app.use(
