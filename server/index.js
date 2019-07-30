bda10af17cb87f85ecc9e5cc1aef922c442faa26
Elmer Gonzalez <elmergonzalez712@gmail.com>
2019-07-29 19:18:43 -0500

    ready for deployment

--- server/index.js
+++ server/index.js
@@ -21,9 +21,14 @@ const port = process.env.PORT || 5000;
 //Static file declaration
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'));
-});
+//production mode
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })
+}
+
+//build mode
+// app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
 
 //set up cors to allows to accept request from the client
 app.use(
